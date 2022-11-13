51ba83dac9c85fa9cd88915526b1abf940d93f25
alisonjordan <alisonjor@gmail.com>
2022-11-13 14:45:46 -0300

    criação e execução do programa em único módulo chamado CarrinhoDeCompras

--- projeto1.rb
+++ projeto1.rb
@@ -1,72 +1,73 @@
-#hash de mensagens para o usuário
-mensagens = { "menu_principal" => ["Selecione a opção desejada: ", "[1] Comprar","[2] Sair"], 
-              "menu_produtos" => ["Selecione a opção desejada: ", "[1]Mouse: R$50,00","[2]Headset: R$200,00","[3]Teclado: R$100,00"],
-              "qtd" => "Digite a quantidade desejada:__",
-              "voltar" => "Digite 0 para voltar ao menu inicial",
-              "bye" => "Obrigado pela preferência!"}
-
-#hash de produtos
-produtos = { "mouse" => 50.00, "headset" => 200.00, "teclado" => 100.00 }
-
-#variável do subtotal
-subtotal = 0.00
-
-#Início do programa
-puts mensagens['menu_principal']
-opcao = gets.chomp.to_i
-
-
-    #Laço de repetição de compra
-    while opcao == 1
-    puts mensagens['menu_produtos']
-    opcao = gets.chomp.to_i
-         
+#Módulo Carrinho de compras
+module CarrinhoDeCompras
+    @@subtotal = 0.00
+    @@mensagens = { "menu_principal" => ["Selecione a opção desejada: ", "[1] Comprar","[2] Sair"], 
+                    "menu_produtos" => ["Selecione a opção desejada: ", "[1]Mouse: R$50,00","[2]Headset: R$200,00","[3]Teclado: R$100,00"],
+                    "qtd" => "Digite a quantidade desejada: ",
+                    "voltar" => "Digite 0 para voltar ao menu inicial: ",
+                    "bye" => "Obrigado pela preferência!"}
+  
+  
+    #Função para chamar o menu principal
+    def menuinicial(opcao)
+      puts @@mensagens['menu_principal']
+      opcao = gets.chomp.to_i
+        if  opcao == 1
+          comprar(opcao)
+        
+        else 
+          puts @@mensagens['bye']
+        end
+        
+    end #fim da função menuinicial
+  
+    #Função para adicionar produtos no carrinho
+    def comprar(opcao)
+    produtos = { "mouse" => 50.00, "headset" => 200.00, "teclado" => 100.00 }
+   
+      while opcao == 1
+        puts @@mensagens['menu_produtos']
+        opcao = gets.chomp.to_i
+        
         case opcao
-        when 1    
-        puts mensagens['qtd']
-        qtd = gets.chomp.to_i
-        subtotal += produtos['mouse'] * qtd
-    
-        puts "Subtotal: R$#{subtotal}"
-        puts mensagens['voltar']
-        opcao = gets.chomp.to_i
-        
-            #condicional para voltar ao menu inicial
-            if opcao == 0
-            puts mensagens['menu_principal']
-            opcao = gets.chomp.to_i
-            end
-        
-        when 2
-        puts mensagens['qtd']
-        qtd = gets.chomp.to_i
-        subtotal += produtos['headset'] * qtd
-        
-        puts "Subtotal: R$#{subtotal}"
-        puts mensagens['voltar']
-        opcao = gets.chomp.to_i
-            
-            if opcao == 0
-            puts mensagens['menu_principal']
-            opcao = gets.chomp.to_i
-            end
-        
-        when 3
-        puts mensagens['qtd']
-        qtd = gets.chomp.to_i
-        subtotal += produtos['teclado'] * qtd
-        
-        puts "Subtotal: R$#{subtotal}"
-        puts mensagens['voltar']
-        opcao = gets.chomp.to_i
-
-            if opcao == 0
-            puts mensagens['menu_principal']
-            opcao = gets.chomp.to_i
-            end
-        end 
-    end #fim do laço
-
-if opcao == 2
-    puts mensagens['bye']
-end
+              when 1    
+              print @@mensagens['qtd']
+              qtd = gets.chomp.to_i
+              @@subtotal += produtos['mouse'] * qtd
+              
+              
+              puts "Subtotal: R$#{@@subtotal}"
+              print @@mensagens['voltar']
+              opcao = gets.chomp.to_i
+              
+              menuinicial(opcao)
+              
+              when 2
+              print @@mensagens['qtd']
+              qtd = gets.chomp.to_i
+              @@subtotal += produtos['headset'] * qtd
+              
+              puts "Subtotal: R$#{@@subtotal}"
+              print @@mensagens['voltar']
+              opcao = gets.chomp.to_i
+              
+              menuinicial(opcao)
+              
+              when 3
+              print @@mensagens['qtd']
+              qtd = gets.chomp.to_i
+              @@subtotal += produtos['teclado'] * qtd
+              
+              puts "Subtotal: R$#{@@subtotal}"
+              print @@mensagens['voltar']
+              opcao = gets.chomp.to_i
+              
+              menuinicial(opcao)
+        end #fim do case
+      end #fim do laço
+    end #fim da função comprar
+  end #fim do módulo
+  
+  #Início do programa
+  include CarrinhoDeCompras
+  menuinicial(nil)
