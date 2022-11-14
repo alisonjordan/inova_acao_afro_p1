b24f5993d5155c62779a7e3c459e68d6d0141d49
Marco Picanço <marcopicanco@live.com>
2022-11-13 21:45:12 -0300

    Listar produtos e valor final ao finalizar a compra

--- projeto1.rb
+++ projeto1.rb
@@ -1,9 +1,10 @@
 #hash de mensagens para o usuário
-mensagens = { "menu_principal" => ["Selecione a opção desejada: ", "[1] Comprar","[2] Sair"], 
-              "menu_produtos" => ["Selecione a opção desejada: ", "[1]Mouse: R$50,00","[2]Headset: R$200,00","[3]Teclado: R$100,00"],
-              "qtd" => "Digite a quantidade desejada:__",
-              "voltar" => "Digite 0 para voltar ao menu inicial",
-              "bye" => "Obrigado pela preferência!"}
+puts "Seja bem-vindo(a). Faça boas compras."
+    mensagens = { "menu_principal" => ["Selecione a opção desejada: ", "\n[1] Comprar","\n[2] Sair"], 
+        "menu_produtos" => ["Selecione a opção desejada: ", "\n[1]Mouse: R$50,00","\n[2]Headset: R$200,00","\n[3]Teclado: R$100,00"],
+        "qtd" => "\nDigite a quantidade desejada: ",
+        "voltar" => "\nDigite [0] para voltar ao menu inicial: ",
+        "bye" => "\nObrigado pela preferência!"}
 
 #hash de produtos
 produtos = { "mouse" => 50.00, "headset" => 200.00, "teclado" => 100.00 }
@@ -13,60 +14,67 @@ subtotal = 0.00
 
 #Início do programa
 puts mensagens['menu_principal']
-opcao = gets.chomp.to_i
+opcao = gets.chomp.to_f
 
 
     #Laço de repetição de compra
     while opcao == 1
     puts mensagens['menu_produtos']
-    opcao = gets.chomp.to_i
+    opcao = gets.chomp.to_f
          
         case opcao
         when 1    
         puts mensagens['qtd']
-        qtd = gets.chomp.to_i
+        qtd = gets.chomp.to_f
         subtotal += produtos['mouse'] * qtd
+        puts "\n #{qtd} Mouses"
     
-        puts "Subtotal: R$#{subtotal}"
+        puts "\nSubtotal: R$#{subtotal}"
         puts mensagens['voltar']
-        opcao = gets.chomp.to_i
+        opcao = gets.chomp.to_f
         
             #condicional para voltar ao menu inicial
             if opcao == 0
             puts mensagens['menu_principal']
-            opcao = gets.chomp.to_i
+            opcao = gets.chomp.to_f
             end
         
         when 2
         puts mensagens['qtd']
-        qtd = gets.chomp.to_i
+        qtd = gets.chomp.to_f
         subtotal += produtos['headset'] * qtd
+        puts "\n #{qtd} Headset"
         
-        puts "Subtotal: R$#{subtotal}"
+        puts "\nSubtotal: R$#{subtotal}"
         puts mensagens['voltar']
-        opcao = gets.chomp.to_i
+        opcao = gets.chomp.to_f
             
             if opcao == 0
             puts mensagens['menu_principal']
-            opcao = gets.chomp.to_i
+            opcao = gets.chomp.to_f
             end
         
         when 3
         puts mensagens['qtd']
-        qtd = gets.chomp.to_i
+        qtd = gets.chomp.to_f
         subtotal += produtos['teclado'] * qtd
+        puts "\n #{qtd} Teclados"
         
-        puts "Subtotal: R$#{subtotal}"
+        puts "\nSubtotal: R$#{subtotal}"
         puts mensagens['voltar']
-        opcao = gets.chomp.to_i
+        opcao = gets.chomp.to_f
 
             if opcao == 0
             puts mensagens['menu_principal']
-            opcao = gets.chomp.to_i
+            opcao = gets.chomp.to_f
             end
         end 
     end #fim do laço
 
-if opcao == 2
-    puts mensagens['bye']
-end
+            if opcao == 2
+                puts mensagens['bye']
+                puts "Seus produtos foram os seguintes:"
+                puts "\n #{qtd} Mouses\n #{qtd} Headsets\n #{qtd} Teclados"
+                puts "\nO valor de sua compra foi de: R$ #{subtotal} Volte sempre!"
+                
+            end
